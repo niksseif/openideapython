13b061d1e92dbd4a548574684caa790a2911a36f
NikSS <nik.safe@gmail.com>
2018-09-16 16:45:57 -0700

    removed import db from app.py and add run.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
-from db import db
+
 
 
 from models.users import UserModel
@@ -30,7 +30,7 @@ from seeds.reviews import reviews
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
